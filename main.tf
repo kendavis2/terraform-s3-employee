c9e2331e99f5da04042205aede90bf3a78d8d08b
kendavis2 <kenneth.davis2@turner.com>
2020-03-24 11:42:54 -0400

    block S3 public access

--- main.tf
+++ main.tf
@@ -37,6 +37,14 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "public_bucket_policy" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
 resource "aws_kms_key" "bucket_key" {
   enable_key_rotation = true
 
